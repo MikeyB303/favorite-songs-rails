21a1475d35d019c3dae8cc58508172c0e550e33a
Mike Bonetti <email": null>
2017-03-28 19:02:38 -0500

    MIKEY B OUT

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 describe User do
+  let(:user) { User.new(username: "MikeyB", email: "test@email.com", password: "password")}
+
+  describe "attributes" do
+    it "has a username" do
+      expect(user.username).to eq("MikeyB")
+    end
+
+    it "has an email" do
+      expect(user.email).to eq("test@email.com")
+    end
+
+    it "has a secure password" do
+      expect(user.password_digest.class).to be(BCrypt::Password)
+    end
+  end
+
 end
